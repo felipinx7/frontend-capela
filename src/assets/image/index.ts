6e5aed3c289d3a544645e4e26ab2fa053cd7be88
Felipe Dev <flimalimafelipek@gmail.com>
2026-08-13 11:18:44 -0300

    feat: added new imagen in project

--- src/assets/image/index.ts
+++ src/assets/image/index.ts
@@ -1,11 +1,7 @@
-import BackgroundBlueLogin from "./background-blue-login.webp";
 import NotFound from "./not-found-result.webp";
 import PhotoDefaultResultFromSearch from "./photo-default-porfile.webp";
 import PhotoLogin from "./photo-login.webp";
+import BackgroundBlueLogin from "./background-blue-login.webp";
+import BackgroundLoginMobile from "./background-login-mobile.webp";
 
-export default [
-  NotFound,
-  PhotoDefaultResultFromSearch,
-  PhotoLogin,
-  BackgroundBlueLogin,
-];
+export { NotFound, PhotoDefaultResultFromSearch, PhotoLogin, BackgroundBlueLogin, BackgroundLoginMobile };
