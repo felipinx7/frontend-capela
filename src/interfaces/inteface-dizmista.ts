e3dca6f6a9c0d6663f28ca07013b670cf31a83b8
Felipe Dev <flimalimafelipek@gmail.com>
2026-08-20 22:00:54 -0300

    feat: create new interfaces in project

---
+++ src/interfaces/inteface-dizmista.ts
@@ -0,0 +1,3 @@
+export interface InterfaceDizimista{
+    nome?: string,
+}
--- src/interfaces/interface-dahboard-results.ts
+++ src/interfaces/interface-dahboard-results.ts
@@ -1,7 +1,8 @@
 import { IconType } from "../types/icon-type";
-
 export interface InterfaceDashboardResult {
-  numberResult: number;
+  numberResult?: number;
   textResult: string;
   icon: IconType;
+  className?: string
+  valueEndSaldo?: number
 }
