90f2c06f1e2670e301ff78f0f938c5392e71a5b2
Felipe Dev <flimalimafelipek@gmail.com>
2026-08-20 22:02:33 -0300

    fix: change name photo default user

--- src/assets/image/index.ts
+++ src/assets/image/index.ts
@@ -1,7 +1,8 @@
 import NotFound from "./not-found-result.webp";
-import PhotoDefaultResultFromSearch from "./photo-default-porfile.webp";
+import PhotoDefaultResultFromSearch from "./not-found-result.webp";
+import PhotoUserDefault from "./photo-default-porfile.webp";
 import PhotoLogin from "./photo-login.webp";
 import BackgroundBlueLogin from "./background-blue-login.webp";
 import BackgroundLoginMobile from "./background-login-mobile.webp";
 
-export { NotFound, PhotoDefaultResultFromSearch, PhotoLogin, BackgroundBlueLogin, BackgroundLoginMobile };
+export { NotFound, PhotoUserDefault, PhotoDefaultResultFromSearch, PhotoLogin, BackgroundBlueLogin, BackgroundLoginMobile };
