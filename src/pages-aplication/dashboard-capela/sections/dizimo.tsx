56522d1afc3877084c07d9ab2c97234d8aadc039
Felipe Dev <flimalimafelipek@gmail.com>
2026-08-28 21:48:04 -0300

    fix: correction in pages and states bad

--- src/pages-aplication/dashboard-capela/sections/dizimo.tsx
+++ src/pages-aplication/dashboard-capela/sections/dizimo.tsx
@@ -2,16 +2,19 @@ import IconAddPeople from "@/src/assets/icons/icon-add-people";
 import IconSearch from "@/src/assets/icons/icon-search";
 import { PhotoDefaultResultFromSearch } from "@/src/assets/image";
 import { CardCreationDizimista } from "@/src/components/layout/modal-creation-dizimista";
+import { ModalDeleteInputValeu } from "@/src/components/layout/modal-delete-input-value";
 import { CardDizimista } from "@/src/components/ui/card-dizimista";
 import { InterfaceDataPorfileDashboard } from "@/src/interfaces/user/interface-data-porfile-dashboard";
 import { DTODizimista } from "@/src/schemas/schema-dizimista";
 import { DTODizimistaUpdate } from "@/src/schemas/schema-dizimista-update";
 import { GetAllDizimista } from "@/src/services/getAllDizimista";
+import { handleStateValeu } from "@/src/utils/handleStateValue";
 import { useEffect, useState } from "react";
 
 export function SectionDizimoCapela({ idCapela }: DTODizimista) {
     const [dizimistas, setDizimista] = useState<InterfaceDataPorfileDashboard[]>([])
     const [openModalCreated, setOpenModalCreated] = useState(false)
+    const [openModalDeleteInputDizimo, setOpenModalDeleteInputDizimo] = useState(false)
     const [openModalUpdate, setOpenModalUpdate] = useState(false)
     const [valueInput, setValeuInput] = useState<string>("")
     const dizimistasFiltered = dizimistas.filter((dizimista) => dizimista.nome?.toLocaleUpperCase().includes(valueInput.toUpperCase()))
@@ -40,6 +43,9 @@ export function SectionDizimoCapela({ idCapela }: DTODizimista) {
         FetchDataDizimo()
     }, []
     )
+
+    console.log("VALOR DO ESTADO NA SEÇÃO DIZIMO", openModalDeleteInputDizimo);
+    
 
     return (
         <section className="style-sections-dashboard gap-4">
@@ -72,6 +78,8 @@ export function SectionDizimoCapela({ idCapela }: DTODizimista) {
                         handleModaAddedMoney={handleModalUpdate}
                         handleModalDelete={handleModalUpdate}
                         idCapela={idCapela}
+                        handleOpenModalDeleteInputDizimo={() => handleStateValeu(setOpenModalDeleteInputDizimo)}
+                        openModalDeleteInputDizimo={openModalDeleteInputDizimo}
                         handleModalUpdate={handleModalUpdate}
                         handleModalView={handleModalUpdate}
                         key={dizimista.nome}
@@ -87,6 +95,7 @@ export function SectionDizimoCapela({ idCapela }: DTODizimista) {
 
             {/* modals used in section  */}
             <CardCreationDizimista idCapela={idCapela} createdDizimista={createdDizimista} handleOpenModal={handleOpenModal} onClosed={openModalCreated} />
+            <ModalDeleteInputValeu OpenModal={openModalDeleteInputDizimo} handleOpenModal={() => handleStateValeu(setOpenModalDeleteInputDizimo)} />
         </section>
     )
 }
