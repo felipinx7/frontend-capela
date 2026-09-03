67baf2a9393120e5d221519e5fd69225c34ec988
Felipe Dev <flimalimafelipek@gmail.com>
2026-09-03 14:04:36 -0300

    fix: change in fuction

--- src/utils/get-mounth-name.ts
+++ src/utils/get-mounth-name.ts
@@ -1,8 +1,22 @@
-export function GetMounthName(numberMounth: number) {
-  if (numberMounth > 0 && numberMounth <= 12) {
-    const mounthUser = new Date(2000, numberMounth - 1);
-    return new Intl.DateTimeFormat("pt-BR", { month: "long" }).format(mounthUser);
+export function GetMounthName(numberMounth: number | Date | string) {
+  let date: Date;
+
+  console.log("params", Number(String(numberMounth).split("-")[1]));
+  console.log("params 2", typeof numberMounth);
+
+
+  if (typeof numberMounth === "number") {
+    date = new Date(2000, numberMounth - 1, 1);
+    console.info("Date if 1", date);
+  } 
+  
+  if(typeof numberMounth === "string"){
+    date = new Date(numberMounth)
   } else {
-    console.log("Mês inválido!!");
+    date = numberMounth as Date
   }
+  
+  console.log("INTL", new Intl.DateTimeFormat("pt-BR", { month: "long" }).format(date))
+
+  return new Intl.DateTimeFormat("pt-BR", { month: "long" }).format(date);
 }
